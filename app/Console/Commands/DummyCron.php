39d8ec464559a92c17167d94b33bb99b4099fb8a
Jose Luis Ferrete <joseluisferrete@gmail.com>
2019-05-05 20:27:31 +0200

    Register the Custom Command

--- app/Console/Commands/DummyCron.php
+++ app/Console/Commands/DummyCron.php
@@ -1,42 +1,19 @@
 <?php
-
 namespace App\Console\Commands;
-
 use Illuminate\Console\Command;
 
 class DummyCron extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'dummy:cron';
+    protected $description = 'Dummy Cron Job Application';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
-        //
+        \Log::info("Running!");
     }
 }
